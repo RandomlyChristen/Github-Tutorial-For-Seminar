23c5ce6715ea5fa6e5c46016cc3d6abcc86759bb
RandomlyChristen <random_lee@naver.com>
2020-07-26 23:46:03 +0900

    소스코드 3줄로 변경

--- test_program.cpp
+++ test_program.cpp
@@ -6,7 +6,5 @@ int main() {
     cout << "내용 2-1" << endl;
     cout << "내용 2-2" << endl;
     cout << "내용 2-3" << endl;
-    cout << "내용 2-4" << endl;
-    cout << "내용 2-5" << endl;
     return 0;
 }
